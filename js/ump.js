ace57ffaa8e12c6d520d73ec361aa6aff252db50
John West <jwest@astreetpress.com>
2011-08-22 13:08:50 -0400

    jQuery (document).ready function that ensures HTML5 video playable. Falls back to flash video.

--- js/ump.js
+++ js/ump.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+	var v = document.createElement("video");
+	if (!v.play)
+	{
+		var params = {
+			allowfullscreen: "true",
+			allowscriptaccess: "always"
+		};
+		var flashvars = {
+			file: "video.f4v",
+			image: "snapshot.jpg"
+		};
+		swfobject.embedSWF("player.swf", "ump", "480", "272", "9.0.0", "expressInstall.swf", flashvars, params);
+	}
 });
